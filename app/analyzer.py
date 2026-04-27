841d46505e4e6619cc65b8acdd97b62b7c7cd397
rahulbharambe <60600560+rahulbharambe@users.noreply.github.com>
2026-04-27 23:00:57 +0530

    Update analyzer.py

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,13 +1,4 @@
-import json
-
-def load_data():
-    with open("data/ticket.json") as f:
-        ticket = json.load(f)
-
-    with open("data/logs.txt") as f:
-        logs = f.read()
-
-    with open("data/code_sample.java") as f:
-        code = f.read()
-
-    return ticket, logs, code
+def detect_pattern(logs):
+    if "NullPointerException" in logs:
+        return "High frequency issue in UserService.java"
+    return "No major pattern detected"
