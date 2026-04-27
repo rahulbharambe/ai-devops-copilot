6d44fa34235275ece06a7ba10a383b0a3a62d8dc
rahulbharambe <60600560+rahulbharambe@users.noreply.github.com>
2026-04-27 23:56:11 +0530

    Update analyzer.py

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,4 +1,16 @@
 def detect_pattern(logs):
-    if "NullPointerException" in logs:
-        return "High frequency issue in UserService.java"
-    return "No major pattern detected"
+    issues = {}
+
+    lines = logs.split("\n")
+
+    for line in lines:
+        if "Exception" in line:
+            file = line.split("at")[-1].strip()
+            issues[file] = issues.get(file, 0) + 1
+
+    if not issues:
+        return "No major patterns detected"
+
+    most_common = max(issues, key=issues.get)
+
+    return f"Most issues found in: {most_common} (occurrences: {issues[most_common]})"
